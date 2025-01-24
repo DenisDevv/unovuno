f163fa7342e710de5e11141eaf09247e2fe428a3
DenisDevv <karajedenis@gmail.com>
2025-01-24 11:28:31 +0100

    effetto danno diminuito di 1 s

--- js/app.js
+++ js/app.js
@@ -242,7 +242,7 @@ socket.on('playerHit', (data) => {
   canvas.classList.add('red-shadow');
   setTimeout(() => {
     canvas.classList.remove('red-shadow');
-  }, 2000);
+  }, 1000);
   if (player.health <= 0) {
     resetGame();
     window.location.href = '/gameover';
